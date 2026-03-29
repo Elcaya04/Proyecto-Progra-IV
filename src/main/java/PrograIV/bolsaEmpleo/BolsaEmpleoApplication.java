65f337bb1274055e58ec44f4cf4567de20678d88
Elcaya04 <gerson.madrigal.cubillo@est.una.ac.cr>
2026-03-29 00:19:11 -0600

    Proyecto casi al 100, falta probar ultimos cambios.

--- src/main/java/PrograIV/bolsaEmpleo/BolsaEmpleoApplication.java
+++ src/main/java/PrograIV/bolsaEmpleo/BolsaEmpleoApplication.java
@@ -3,8 +3,6 @@ package PrograIV.bolsaEmpleo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Esta anotación es vital: le dice a Spring que arranque el servidor
-// y que busque las @Entity dentro de la carpeta Logic.
 @SpringBootApplication
 public class BolsaEmpleoApplication {
 
